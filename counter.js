bd6ba1714b818d18cabcd9adab3f0a7141d2e654
Diana H. Rendon <144194733+dianaRendon@users.noreply.github.com>
2024-09-16 10:50:28 -0600

    Delete counter.js

--- counter.js
+++
@@ -1,8 +0,0 @@
-const countVisit = async () => {
-    const response = await fetch('https://api.countapi.xyz/hit/tu-dominio.com/visitas');
-    const data = await response.json();
-    document.getElementById('visit-count').innerText = `Visitas: ${data.value}`;
-  };
-  
-  document.addEventListener('DOMContentLoaded', countVisit);
-  

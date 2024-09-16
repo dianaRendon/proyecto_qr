5a1bc69329ea1f91fb039d0671c1db83a9d5712c
Diana H. Rendon <144194733+dianaRendon@users.noreply.github.com>
2024-09-16 11:47:17 -0600

    Add files via upload

--- js/counter.js
+++ js/counter.js
@@ -1,7 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', async function() {
   const visitasElemento = document.getElementById('visitas');
-  let visitas = localStorage.getItem('visitas') || 0;
+  const binUrl = 'https://api.jsonbin.io/v3/b/66e86a9dad19ca34f8a71cd1'; 
+
+  // Obtener el número de visitas
+  let response = await fetch(binUrl, {
+    method: 'GET',
+    headers: {
+      'X-Master-Key': '$2a$10$cYXg0yUGh4epl0BDX/M/LeBnGkatzvFFMTJWZw6M7cLnZL8CLSN.q', // Inserta tu API Key aquí
+      'Content-Type': 'application/json'
+    }
+  });
+  let data = await response.json();
+  let visitas = data.record.visitas;
+
+  // Incrementar y mostrar el número de visitas
   visitas++;
-  localStorage.setItem('visitas', visitas);
   visitasElemento.textContent = visitas;
+
+  // Guardar el nuevo número de visitas en JSONBin
+  await fetch(binUrl, {
+    method: 'PUT',
+    headers: {
+      'X-Master-Key': '$2a$10$cYXg0yUGh4epl0BDX/M/LeBnGkatzvFFMTJWZw6M7cLnZL8CLSN.q', // Inserta tu API Key aquí
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ visitas: visitas })
+  });
 });
+
